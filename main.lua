6c019eb762cf41ebf8397825fbe85a5261763595
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2024-02-03 18:11:54 +0300

    variables

--- main.lua
+++ main.lua
@@ -1,5 +1,13 @@
+message = "Imma go on my way!"
+myNumber = 23
+chickenNum = 5
+output = chickenNum + 18 -- 23
+
+myNumber = myNumber + 1 -- 24
+
+
 function love.draw()
   -- set font size to 50
   love.graphics.setFont(love.graphics.newFont(50))
-  love.graphics.print("Hello World!")
+  love.graphics.print(myNumber)
 end
