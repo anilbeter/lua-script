9cc916fe11bd26872fb31573efd2c12d6380afa0
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2024-02-03 18:59:00 +0300

    functions

--- main.lua
+++ main.lua
@@ -1,27 +1,22 @@
 message = 0
-test = 0
-pickle = 0
-hehe = 5
 
-while message < 100 do
-    message = message + 1
-    test = test - 5
+function increaseMessage(num)
+    message = message + num
 end
 
--- i-> iterator. 3-> when i reached to this value(3), loop will be stopped. 1-> i increases by 1 at every iteration 
-for i = 1, 3, 1 do
-    pickle = pickle + 10
-end
--- i=1 ile başladı ve i=3'e kadar loop devam edecek ve her iteration da 1 artıyor çünkü 1 belirledim. her iteration da pickle 10 artıyor, 0 ile başladı loop sonunda 30 olucak
+increaseMessage(23)
+-- now message equals to 23
 
-for i = 10, 5, -1 do
-    hehe = hehe + 2
+function double(val)
+    val = val * 2
+    return val
 end
--- end of the loop hehe will be 17
--- loop iterates 6 times; 10, 9, 8, 7, 6, and 5 then will be over
+
+message = double(message)
+-- now message equals to 46
 
 function love.draw()
     -- set font size to 50
     love.graphics.setFont(love.graphics.newFont(50))
-    love.graphics.print(pickle) -- 30
+    love.graphics.print(message) -- 46
 end
