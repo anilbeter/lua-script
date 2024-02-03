8a9402f86a7bcf31ff902632b2c8c77238b4ee13
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2024-02-03 18:21:50 +0300

    else and elseif

--- main.lua
+++ main.lua
@@ -1,16 +1,16 @@
 myNumber = 23
-condition = 25
+condition = -55
 
 if condition > 0 then
   myNumber = 0
-end
-
-if condition <= 0 then
-  myNumber = -1
+elseif condition < -50 then
+  myNumber = 73
+else
+  myNumber = 100
 end
 
 function love.draw()
   -- set font size to 50
   love.graphics.setFont(love.graphics.newFont(50))
-  love.graphics.print(myNumber)
+  love.graphics.print(myNumber) -- 73
 end
