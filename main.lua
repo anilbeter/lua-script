5e1670ab4a8ee266e03cca61e19eb54ffa98feec
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2024-02-04 15:15:28 +0300

    for loop w/ tables

--- main.lua
+++ main.lua
@@ -1,28 +1,19 @@
 message = 0
 
--- variables store 1 piece of data
--- tables, store multiple pieces of data
-
--- creating table (empty table atm)
-anilFavs = {}
-anilFavs[1] = 23
-anilFavs[2] = "Maybe l.a. dream?"
-anilFavs[3] = "dreams come through"
--- diğer proglama dilleri gibi index 0 dan başlamıyor, index 1den başlıyor
-
--- another way of creating table
 testScores = {23, 99, 105}
-print(testScores[1]) -- 23
-testScores["math"] = 91
-print(testScores["math"]) -- 91
 
--- add new elements into a table w/ insert function
-table.insert(anilFavs, "Still thinking about.")
+-- tables with for loop
 
-message = anilFavs[4]
+-- i = index, s = value
+for i, s in ipairs(testScores) do
+    message = message + s
+end
+-- first iteration -> i = 1, s = 23, message = 23
+-- second iteration -> i = 2, s = 99, message = 23 + 99
+-- third iteration -> i = 3, s = 105, message = 105 + 23 + 99
 
 function love.draw()
     -- set font size to 50
     love.graphics.setFont(love.graphics.newFont(50))
-    love.graphics.print(message)
+    love.graphics.print(message) -- 227
 end
