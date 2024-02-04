ff49837a45a80f9ae3a7ed722c6abb3f41b23d8c
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2024-02-04 15:11:17 +0300

    tables pt1.

--- main.lua
+++ main.lua
@@ -1,12 +1,25 @@
--- global variable
 message = 0
 
-function getHalf(i)
-    -- local variable
-    local var = i
-    var = var / 2
-    return var
-end
+-- variables store 1 piece of data
+-- tables, store multiple pieces of data
+
+-- creating table (empty table atm)
+anilFavs = {}
+anilFavs[1] = 23
+anilFavs[2] = "Maybe l.a. dream?"
+anilFavs[3] = "dreams come through"
+-- diğer proglama dilleri gibi index 0 dan başlamıyor, index 1den başlıyor
+
+-- another way of creating table
+testScores = {23, 99, 105}
+print(testScores[1]) -- 23
+testScores["math"] = 91
+print(testScores["math"]) -- 91
+
+-- add new elements into a table w/ insert function
+table.insert(anilFavs, "Still thinking about.")
+
+message = anilFavs[4]
 
 function love.draw()
     -- set font size to 50
