b68670dd00df743901ca832a919e8b4a2777bd47
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2024-02-04 14:52:53 +0300

    local and global variables

--- main.lua
+++ main.lua
@@ -1,22 +1,15 @@
+-- global variable
 message = 0
 
-function increaseMessage(num)
-    message = message + num
+function getHalf(i)
+    -- local variable
+    local var = i
+    var = var / 2
+    return var
 end
-
-increaseMessage(23)
--- now message equals to 23
-
-function double(val)
-    val = val * 2
-    return val
-end
-
-message = double(message)
--- now message equals to 46
 
 function love.draw()
     -- set font size to 50
     love.graphics.setFont(love.graphics.newFont(50))
-    love.graphics.print(message) -- 46
+    love.graphics.print(message)
 end
