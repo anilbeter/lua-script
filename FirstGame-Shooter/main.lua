cc20344af39174aa27468eb763928bbc119b028d
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2024-02-06 10:26:41 +0300

    adjust font size

--- FirstGame-Shooter/main.lua
+++ FirstGame-Shooter/main.lua
@@ -6,6 +6,9 @@ function love.load()
 
     score = 0
     timer = 0
+
+    -- new font size
+    gameFont = love.graphics.newFont(40)
 end
 
 function love.update(dt)
@@ -15,4 +18,8 @@ end
 function love.draw()
     love.graphics.setColor(1, 0, 0)
     love.graphics.circle("fill", target.x, target.y, target.radius)
+
+    love.graphics.setColor(1, 1, 1)
+    love.graphics.setFont(gameFont)
+    love.graphics.print(score, 0, 0)
 end
