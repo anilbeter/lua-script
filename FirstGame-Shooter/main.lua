1fbbf824fc9e8a6c1642cf1896dd2976bff81f22
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2024-02-06 10:23:28 +0300

    create target table and global variables

--- FirstGame-Shooter/main.lua
+++ FirstGame-Shooter/main.lua
@@ -1,5 +1,11 @@
 function love.load()
-    
+    target = {}
+    target.x = 300
+    target.y = 300
+    target.radius = 50
+
+    score = 0
+    timer = 0
 end
 
 function love.update(dt)
@@ -7,10 +13,6 @@ function love.update(dt)
 end
 
 function love.draw()
-    -- setcolor(r,g,b)
-    love.graphics.setColor(0, 1, 1)
-    love.graphics.rectangle("fill", 200, 400, 200, 100)
-   
-    love.graphics.setColor(204/255, 12/255, 23/255)
-    love.graphics.circle("fill", 100, 100, 100)
+    love.graphics.setColor(1, 0, 0)
+    love.graphics.circle("fill", target.x, target.y, target.radius)
 end
