1fc090320f205579512ead77fff146fff8f97543
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2024-02-06 10:18:37 +0300

    set color

--- FirstGame-Shooter/main.lua
+++ FirstGame-Shooter/main.lua
@@ -1,14 +1,16 @@
 function love.load()
-    number = 0
+    
 end
 
 function love.update(dt)
-    number = number + 1
+    
 end
 
 function love.draw()
-    -- mode, x, y, width, height
+    -- setcolor(r,g,b)
+    love.graphics.setColor(0, 1, 1)
     love.graphics.rectangle("fill", 200, 400, 200, 100)
-    -- mode, x, y, radius
+   
+    love.graphics.setColor(204/255, 12/255, 23/255)
     love.graphics.circle("fill", 100, 100, 100)
 end
