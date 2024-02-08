79b146fa3d8b9e4ef72acd47d3a128d169dac3dc
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2024-02-08 11:24:32 +0300

    spawn enemies

--- TopDownShooter/main.lua
+++ TopDownShooter/main.lua
@@ -13,7 +13,8 @@ function love.load()
   player.speed = 180
   -- in love.update, player.speed will be 3 (180 / 60 = player.speed * 1/60(dt))
 
-  tempRotation = 0
+  zombies = {}
+
 end
 
 function love.update(dt)
@@ -29,17 +30,36 @@ function love.update(dt)
   if love.keyboard.isDown("s") then
     player.y = player.y + player.speed * dt
   end
-
-  tempRotation = tempRotation + 0.01
 end
 
 function love.draw()
   love.graphics.draw(sprites.background, 0, 0)
   love.graphics.draw(sprites.player, player.x, player.y, playerMouseAngle(), nil, nil, player.width/2, player.height/2)
   -- player offset origini tam merkeze aldım, artık player kendi etrafında dönüyor
+
+  for i,z in ipairs(zombies) do
+    love.graphics.draw(sprites.zombie, z.x, z.y)
+  end
+end
+
+function love.keypressed(key)
+  if key == "space" then
+    spawnZombie()
+  end
 end
 
 function playerMouseAngle()
   -- formula -> atan2(y1-y2, x1-x2)
   return math.atan2(player.y - love.mouse.getY(), player.x - love.mouse.getX()) + math.pi
+end
+
+function spawnZombie()
+  local zombie = {}
+  zombie.width = sprites.zombie:getWidth()
+  zombie.height = sprites.zombie:getHeight()
+  zombie.x = math.random(0, love.graphics.getWidth() - zombie.width)
+  zombie.y = math.random(0, love.graphics.getHeight() - zombie.height)
+  zombie.speed = 100
+  
+  table.insert(zombies, zombie)
 end
