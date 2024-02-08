b41b5d6084ec45110fac7045f17d7fee9398f366
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2024-02-08 11:02:28 +0300

    sprite rotation

--- TopDownShooter/main.lua
+++ TopDownShooter/main.lua
@@ -12,6 +12,8 @@ function love.load()
   player.y = love.graphics.getHeight() / 2 - player.height/2
   player.speed = 180
   -- in love.update, player.speed will be 3 (180 / 60 = player.speed * 1/60(dt))
+
+  tempRotation = 0
 end
 
 function love.update(dt)
@@ -27,9 +29,12 @@ function love.update(dt)
   if love.keyboard.isDown("s") then
     player.y = player.y + player.speed * dt
   end
+
+  tempRotation = tempRotation + 0.01
 end
 
 function love.draw()
   love.graphics.draw(sprites.background, 0, 0)
-  love.graphics.draw(sprites.player, player.x, player.y)
+  love.graphics.draw(sprites.player, player.x, player.y, tempRotation, nil, nil, player.width/2, player.height/2)
+  -- player offset origini tam merkeze aldım, artık player kendi etrafında dönüyor
 end
