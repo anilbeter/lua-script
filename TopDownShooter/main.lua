8068ac426cfc9f04bcdce0a78e0d7c4b89187d5c
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2024-02-08 19:43:10 +0300

    delete projectiles when out of screen

--- TopDownShooter/main.lua
+++ TopDownShooter/main.lua
@@ -46,6 +46,14 @@ function love.update(dt)
     b.x = b.x + math.cos(b.direction) * b.speed * dt
     b.y = b.y + math.sin(b.direction) * b.speed * dt
   end
+
+  -- #bullets -> length/size of bullets table
+  for i=#bullets, 1, -1 do
+    local b = bullets[i]
+    if b.x < 0 or b.y < 0 or b.x > love.graphics.getWidth() or b.y > love.graphics.getHeight() then
+      table.remove(bullets, i)
+    end
+  end
 end
 
 function love.draw()
