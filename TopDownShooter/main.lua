73fdafcacc2d66915e2f784cee73e2edc6ae2272
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2024-02-10 08:51:48 +0300

    set zombie spawn position to screen offset

--- TopDownShooter/main.lua
+++ TopDownShooter/main.lua
@@ -116,11 +116,26 @@ function spawnZombie()
   local zombie = {}
   zombie.width = sprites.zombie:getWidth()
   zombie.height = sprites.zombie:getHeight()
-  zombie.x = math.random(0, love.graphics.getWidth() - zombie.width)
-  zombie.y = math.random(0, love.graphics.getHeight() - zombie.height)
+  zombie.x = 0
+  zombie.y = 0
   zombie.speed = 140
   zombie.dead = false
   
+  local side = math.random(1, 4)
+  if side == 1 then
+    zombie.x = -30
+    zombie.y = math.random(0, love.graphics.getHeight())
+  elseif side == 2 then
+    zombie.x = love.graphics.getWidth() + 30
+    zombie.y = math.random(0, love.graphics.getHeight())
+  elseif side == 3 then
+    zombie.x = math.random(0, love.graphics.getWidth())
+    zombie.y = -30
+  elseif side == 4 then
+    zombie.x = math.random(0, love.graphics.getWidth())
+    zombie.y = love.graphics.getHeight() + 30
+  end
+
   table.insert(zombies, zombie)
 end
 
